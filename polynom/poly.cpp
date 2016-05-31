d50519a95c555b522153400f7958b3d07b4eaf4d
salameh <eng_mohammad_Salameh@yahoo.com>
2016-05-31 13:22:06 +0200

    not complete solution

--- polynom/poly.cpp
+++ polynom/poly.cpp
@@ -13,16 +13,20 @@
 // TODO: implement your function(s) for your Module here
 double compute_poly(const unsigned int poly_degree_1,const double *poly_coeff_1,double x)
 {
-	double Var1;//Var2;
+	double Var1 , Var2=0;//Var2;
+        double Xip = 1;
     //sc_out<double> F;
-        Var1 = poly_coeff_1[0];
-        for (int i=1 ;i<=poly_degree_1;i++)
+        for (int i=0 ;i<=poly_degree_1;i++)
 	{
-        Var1+=poly_coeff_1[poly_degree_1]*x;
+                Var1 = poly_coeff_1[poly_degree_1]*Xip;
+                Var2=x*Xip;
+                Xip=Var2;
+                Var3=Y+Var1;
+                Y=Var3;
 	}
         //Var1=5*x;
 	//Var2=2*x*x;
 	//double F=Var2-Var1+5;
 	
-    return Var1;
+    return Var2;
 }
