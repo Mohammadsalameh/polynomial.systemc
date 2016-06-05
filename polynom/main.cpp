0807c2224b62285fa41a99ef51185c2d8e19dd59
salameh <eng_mohammad_Salameh@yahoo.com>
2016-06-05 11:18:44 +0200

    the last version got from the solution files

--- polynom/main.cpp
+++ polynom/main.cpp
@@ -1,16 +1,16 @@
 // Copyright (C) 2014 University of Kaiserslautern
 // Microelectronic System Design Research Group
-// 
+//
 // This file is part of the FPGAHS Course
 // de.uni-kl.eit.course.fpgahs
-// 
+//
 // Matthias Jung <jungma@eit.uni-kl.de>
 // Christian De Schryver <schryver@eit.uni-kl.de>
 // Markus Steffes <steffesm@rhrk.uni-kl.de>
 
 #include <iostream>
 #include "systemc.h"
-#include "poly.h"
+#include <poly.h>
 #include "stim_polynom.h"
 #include "mon_polynom.h"
 
@@ -18,19 +18,16 @@ int sc_main(int argc, char* argv[])
 {
   sc_signal<double> XSig, YSig;
 
-// a clock that has a positive edge only at 10 ns 
+// a clock that has a positive edge only at 10 ns
   sc_clock TestClk("TestClock", 10, SC_NS,0.5, 10, SC_NS);
 
   stim_polynom Stim1("Stimulus");
   Stim1.X(XSig); 			// Module Output X
   Stim1.Clk(TestClk);		// Module Input Clock
 
-// TODO: use your module here
-// Input  Signal: XSig
-// Output Signal: YSig
-  poly DUT("poly");
-  DUT.X(XSig);
-  DUT.Y(YSig);
+  ModPolyEval DUT("DUT_PolyEval");
+  DUT.X(XSig);				// Module Input X
+  DUT.Y(YSig);				// Module Output Y
 
   mon_polynom Monitor1("Monitor");
   Monitor1.X(XSig);			// Module Input X
@@ -42,9 +39,3 @@ int sc_main(int argc, char* argv[])
   return 0;
 
 }
-
-// int main(int argc, char* argv[])
-// {
-// 	// return test_sw();
-// 	return test_sc(argc, argv);
-// }
